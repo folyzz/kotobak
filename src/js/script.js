e3327c5c33e2e0da54aa636b18120890aae2b0a1
Ammar Badie <folyzz11@gmail.com>
2019-02-25 21:07:45 +0300

    add last section

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+$(window).scroll(function(){
+    if($(this).scrollTop()>=600){
+        $("#navBar").addClass("noTransparrent");
+    }
+    else{
+        $("#navBar").removeClass("noTransparrent");
+    }
+});
+
+$(document).ready(function(){
+    $("a.scroll").on('click',function(event){
+        var hash = this.hash;
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){});
+    });
+
+    
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var typed = new Typed('#search', {
+        strings: ["العمل عبر الإنترنت", "تجربة المستخدم UX"],
+        typeSpeed: 50,
+        backSpeed: 30
+    });
+});
