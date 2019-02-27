1152db68c6081d96c76bdd75baaafd551b51eb82
Ammar Badie <folyzz11@gmail.com>
2019-02-27 18:06:20 +0300

    add freelance page header

--- src/js/script.js
+++ src/js/script.js
@@ -13,14 +13,33 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){});
     });
 
-    
     new WOW().init();
     
     $('[data-toggle="tooltip"]').tooltip();
 
+    $('[data-add-to-cart]').click(function(e){
+        alert('أضيف المُنتج إلى عربة الشراء');
+        e.stopPropagation();
+    });
+
+    $('[data-add-to-favorite]').click(function(e){
+        alert('أضيف المُنتج إلى المفضلة');
+    });
+
+    
     var typed = new Typed('#search', {
         strings: ["العمل عبر الإنترنت", "تجربة المستخدم UX"],
         typeSpeed: 50,
         backSpeed: 30
+    });
+});
+
+$(function(){
+    $('input[type="number"]').niceNumber({
+        autoSize: false,
+        autoSizeBuffer: 1,
+        buttonDecrement:'-',
+        buttonIncrement:'+',
+        buttonPosition: 'around'
     });
 });
