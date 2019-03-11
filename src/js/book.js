08f1716b5750ee210bc283e3b70865e6a6b851c2
Ammar Badie <folyzz11@gmail.com>
2019-03-11 19:38:40 +0300

    change all navbars

--- src/js/book.js
+++ src/js/book.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $("a.scroll").on('click',function(event){
         var hash = this.hash;
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,19 @@
+$(window).scroll(function(){
+    if($(this).scrollTop()>=150){  
+        $("#navBar").addClass("noTransparrent"); 
+    }
+    else{
+        $("#navBar").removeClass("noTransparrent");
+    }
+});
 $(document).ready(function(){
     $("a.scroll").on('click',function(event){
         var hash = this.hash;
         $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){});
     });
-
+    
     $('[data-toggle="tooltip"]').tooltip();
-     
+    
     new WOW().init();
     
     var typed = new Typed('#search', {
