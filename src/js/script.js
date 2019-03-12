42451214a9bb406f342e1e4ad24e2a4e1e155c41
Ammar Badie <folyzz11@gmail.com>
2019-03-12 20:59:31 +0300

    division of work

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,17 @@
-$(window).scroll(function(){
-    if($(this).scrollTop()>=150){  
-        $("#navBar").addClass("noTransparrent"); 
-    }
-    else{
-        $("#navBar").removeClass("noTransparrent");
-    }
-});
+
 $(document).ready(function(){
     $("a.scroll").on('click',function(event){
         var hash = this.hash;
         $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){});
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>=150){  
+            $("#navBar").addClass("noTransparrent"); 
+        }
+        else{
+            $("#navBar").removeClass("noTransparrent");
+        }
     });
     
     $('[data-toggle="tooltip"]').tooltip();
